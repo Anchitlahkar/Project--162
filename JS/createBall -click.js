fa703c717bcc2ef2db9d58169c3fbe8fe3f2a587
Anchit Lahkar <64302945+Anchitlahkar@users.noreply.github.com>
2022-03-24 13:09:40 +0530

    24.03.2022

--- JS/createBall -click.js
+++ JS/createBall -click.js
@@ -9,6 +9,8 @@ AFRAME.registerComponent("create-ball", {
   throwBall: function () {
     window.addEventListener("click", (e) => {
       
+      if(e){
+      
         var ball = document.createElement("a-entity");
 
         ball.setAttribute("geometry", {
@@ -44,7 +46,7 @@ AFRAME.registerComponent("create-ball", {
         var scene = document.querySelector("#scene");
 
         scene.appendChild(ball);
-      
+      }
     });
   },
 
